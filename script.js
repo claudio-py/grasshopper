ff9d0da6a30fb585601c1deee4a760212c0f0abe
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-06-03 20:42:59 -0300

    tenth commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const imgElements = document.querySelectorAll('.card-box')
-const zoom = document.querySelector('.zoom')
+
+
 
 imgElements.forEach(function (imgElement) {
 	imgElement.addEventListener('click', function () {
-  for(let element of imgElements){
-    element.classList.remove('zoom')
-  }
-  imgElement.classList.toggle('zoom')
+	
+    this.classList.toggle('zoom')
 	})
 })
